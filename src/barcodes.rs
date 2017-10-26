5c17b23bba00e83e1bb45303ebdeb58712afbf7d
Denys Vitali <denys.vitali@coelis.ch>
2017-10-27 00:29:36 +0200

    Implement OpenFood API

--- src/barcodes.rs
+++ src/barcodes.rs
@@ -1,9 +1,12 @@
-pub  mod barcode {
+
+pub mod barcode {
     #[derive(Debug, PartialEq)]
     pub enum BarcodeType {
         Code128,
         Pharmacode39,
+        EAN8,
         EAN13,
+        QR,
         Unknown
     }
 
@@ -13,5 +16,17 @@ pub  mod barcode {
             '#' => BarcodeType::Code128,
             _ => BarcodeType::Unknown
         }
+    }
+
+    pub fn detect_type(s : &str) -> BarcodeType{
+        if s.starts_with("FF") {
+            return BarcodeType::EAN8;
+        }
+
+        if s.starts_with("QR") {
+            return BarcodeType::QR;
+        }
+
+        return from_char(s.chars().nth(0).unwrap());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,77 @@
 extern crate serial;
+extern crate serde_yaml;
+extern crate serde_json;
+
+// HTTP Client
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
 
 mod barcodes;
 
 use barcodes::barcode;
 use barcodes::barcode::BarcodeType;
 
-use std::io;
-use std::io::ErrorKind;
-use std::fmt::Write;
+use std::io::{self, Write, Read, ErrorKind};
+use futures::{Future, Stream};
+use std::str;
+use hyper::Client;
+use tokio_core::reactor::Core;
+use hyper::{Method, Request};
+use hyper::header::{ContentLength, ContentType,Authorization};
+use hyper_tls::HttpsConnector;
+
+use serde_json::Value;
+
+use std::error::Error;
+
+use std::collections::BTreeMap;
+
+use std::fs::File;
 
 use std::time::Duration;
 use serial::prelude::*;
 
+static SETTINGS_FILE : &'static str = "settings.yml";
+
 fn main() {
+    let config: BTreeMap<String,String>;
+
+    match load_config() {
+        Ok(t) => {
+            config = t;
+            println!("Configuration loaded succesfully!");
+        }
+        Err(e) => {
+            println!("Unable to load configuration! {:?}", e.kind());
+            return;
+        }
+    }
+
+    let mut openfood_apikey : &str;
+    match config.get("openfood_apikey") {
+        None => {
+            println!("OpenFood API key not found!");
+            return;
+        },
+        Some(t) => {
+            openfood_apikey = t;
+        }
+    }
+
+    println!("OpenFood API key: {}", openfood_apikey);
+
+    //testRequest("7613331649478", openfood_apikey);
+
     // Open Serial
     let mut port = serial::open("/dev/ttyACM0").unwrap();
     loop {
         match interact(&mut port) {
-            Ok(_result) => {},
+            Ok(result) => {
+                println!("Barcode from interact: {}", result);
+                //testRequest(&result, openfood_apikey);
+            },
             Err(e) => {
                 if e.kind() == ErrorKind::TimedOut {
                     println!("Timed Out");
@@ -30,7 +84,69 @@ fn main() {
     }
 }
 
-fn interact<T: SerialPort>(port: &mut T) -> io::Result<()>{
+fn load_config() -> Result<BTreeMap<String,String>,std::io::Error>{
+    let mut path = String::from("./");
+    path.push_str(SETTINGS_FILE);
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let yaml : BTreeMap<String, String> = serde_yaml::from_str(&mut contents).unwrap();
+    //println!("OpenFood API key: {}", yaml.get("openfood_apikey").unwrap());
+    Ok(yaml)
+}
+
+fn testRequest(barcode: &str, key: &str) -> Result<(), Box<Error>>{
+    let mut core = Core::new()?;
+    let handle = core.handle();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle)?)
+        .build(&handle);
+
+    let mut json = r#"{
+        "_source": {
+            "includes": [
+                "name_translations",
+                "barcode"
+            ]
+        },
+        "size": 20,
+        "query": {
+            "query_string": {
+                "fields" : [
+                    "barcode"
+                ],
+                "query" : "BARCODE_HERE"
+            }
+        }
+    }
+    "#;
+
+    let json : String = json.replace("BARCODE_HERE", barcode);
+
+    let uri = "https://www.openfood.ch/api/v3/products/_search".parse()?;
+    let mut req : Request = Request::new(Method::Post, uri);
+    req.headers_mut().set(ContentType::json());
+    req.headers_mut().set(ContentLength(json.len() as u64));
+    req.headers_mut().set(Authorization(format!("Token token={}", key)));
+    req.set_body(json);
+    let post = client.request(req).and_then(|res| {
+        println!("POST: {}", res.status());
+        res.body().concat2()
+    });
+    let posted = core.run(post).unwrap();
+    let json_body : Value = serde_json::from_str(&String::from_utf8(posted.to_vec())?)?;
+    let first_result : &Value = json_body["hits"]["hits"].get(0).unwrap();
+    let names = first_result["_source"]["name_translations"].as_object().unwrap();
+    println!("First result: {:?}", first_result);
+    println!("Name en: {:?}", names.get("en"));
+    println!("Name it: {:?}", names.get("it"));
+    println!("Name de: {:?}", names.get("de"));
+    println!("Name fr: {:?}", names.get("fr"));
+    Ok(())
+}
+
+fn interact<T: SerialPort>(port: &mut T) -> io::Result<String>{
     port.reconfigure(&|settings| {
         settings.set_baud_rate(serial::Baud9600)?;
         settings.set_char_size(serial::Bits8);
@@ -56,8 +172,10 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()>{
             result.push(*x);
         }
     }
-
-    let barcode_type = barcode::from_char(char::from(*result.get(0).unwrap()));
+    let mut vec = result.clone();
+    vec.clone_from_slice(&result);
+    let barcode : String = String::from_utf8(vec).unwrap();
+    let barcode_type = barcode::detect_type(&barcode);
     let mut s = String::new();
     println!("barcode type: {:?}", barcode_type);
 
@@ -65,20 +183,13 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()>{
         // Clean Barcode
         result.remove(0);
     }
-
-    println!("Buf: {:?}", &result);
-    for &byte in &result{
-        write!(&mut s, "{:X} ", byte).expect("Unable to write");
+    else {
+        result.drain(0..2);
     }
 
-    println!("Hex: {}", s);
-
-    s = match String::from_utf8(result) {
-        Ok(t) => t,
-        _ => String::from("N/A")
-    };
+    s = String::from_utf8(result).unwrap();
 
     println!("barcode: {:?}", s);
 
-    Ok(())
+    Ok(s)
 }
